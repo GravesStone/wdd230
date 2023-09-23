bc80e122775765569c2419cb156bde6d14c416fc
GravesStone <mark2899estimada@gmail.com>
2023-09-23 18:32:10 +0800

    all sync

---
+++ week-1/scripts/function.js
@@ -0,0 +1,5 @@
+const y = new Date();
+let year = y.getFullYear();
+const date = new Object (document.lastModified);
+
+document.getElementById("year").innerHTML = `&copy;${year} | Mark Augustine Estimada | Last Updated: ${date}` 

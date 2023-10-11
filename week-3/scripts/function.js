97253cbe0ee03ae7552d2641bd48ffca6c6c6e0a
GravesStone <est22003@byui.edu>
2023-10-12 00:21:39 +0800

    last sync

--- week-3/scripts/function.js
+++ week-3/scripts/function.js
@@ -1,5 +1,14 @@
-const realdate = new Date()
-document.querySelector("#year").innerHTML = realdate.getFullYear();
-
-
-
+var dateobj = new Date();{
+    // Year from the above object 
+           // is being fetched using getFullYear() 
+           var dateObject = dateobj.getFullYear();
+           
+           // Printing current year 
+           document.getElementById("copyright").innerHTML ="&copy" + dateObject;
+   };
+          
+   
+   var update = "last update: ";
+   const mdates = new Date(document.lastModified);
+   document.getElementById("lastModified").innerHTML = update + mdates;
+   

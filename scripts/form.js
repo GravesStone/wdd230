1401f4fe90b38dd93ecf5773552a4523fe150981
GravesStone <est22003@byui.edu>
2023-11-02 01:25:19 +0800

    sync it on

--- scripts/form.js
+++ scripts/form.js
@@ -8,37 +8,23 @@ range.addEventListener('input', displayRatingValue);
 function displayRatingValue() {
     rangevalue.innerHTML = range.value;
 }
+function updateRatingValue(input) {
+    document.getElementById("ratingValue").textContent = input.value;
+}
 
-function checkPassword(input) {
+// JavaScript function to check if passwords match
+function checkPassword() {
     const password = document.getElementById("password").value;
-    const confirmPassword = input.value;
-    if (password !== confirmPassword) {
+    const confirm_password = document.getElementById("confirm_password").value;
+
+    if (password !== confirm_password) {
         alert("Passwords do not match. Please try again.");
         document.getElementById("username").value = "";
         document.getElementById("password").value = "";
         document.getElementById("confirm_password").value = "";
         document.getElementById("username").focus();
+        return false; // Prevent form submission
     }
+
+    return true; // Allow form submission
 }
-
-// JavaScript function to update the displayed rating value
-function updateRatingValue(input) {
-    document.getElementById("ratingValue").textContent = input.value;
-}
-
-// JavaScript to toggle password visibility
-const togglePasswordButtons = document.querySelectorAll(".toggle-password");
-togglePasswordButtons.forEach(button => {
-    button.addEventListener("click", function () {
-        const targetId = this.getAttribute("data-target");
-        const targetInput = document.getElementById(targetId);
-
-        if (targetInput.type === "password") {
-            targetInput.type = "text"; // Show the password
-            this.innerHTML = '<i class="fas fa-eye-slash"></i>';
-        } else {
-            targetInput.type = "password"; // Hide the password
-            this.innerHTML = '<i class="fas fa-eye"></i>';
-        }
-    });
-});

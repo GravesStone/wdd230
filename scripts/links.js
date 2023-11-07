0b3eac0acde8c2b0efb76e4a0aaec99df4f379d7
GravesStone <est22003@byui.edu>
2023-11-07 13:49:07 +0800

    changes

--- scripts/links.js
+++ scripts/links.js
@@ -1,1 +1,35 @@
-const baseUrl ="https://gravesstone.github.io/wdd230/";
+const baseURL = "https://gravesstone.github.io/wdd230/"; // Replace with your actual GitHub Pages URL
+        const linksURL = `${baseURL}data/links.json`;
+
+        async function getLinks() {
+            const response = await fetch(linksURL);
+            const data = await response.json();
+            displayLinks(data);
+        }
+
+        function displayLinks(weeks) {
+            const linksContainer = document.getElementById('links-container');
+
+            for (const week of weeks.lessons) {
+                const weekTitle = `Week ${week.lesson}`;
+                const linksList = document.createElement('ul');
+
+                for (const link of week.links) {
+                    const listItem = document.createElement('li');
+                    const anchor = document.createElement('a');
+                    anchor.href = link.url;
+                    anchor.textContent = link.title;
+                    listItem.appendChild(anchor);
+                    linksList.appendChild(listItem);
+                }
+
+                const weekContainer = document.createElement('div');
+                weekContainer.className = 'week-container';
+                weekContainer.innerHTML = `<h2>${weekTitle}</h2>`;
+                weekContainer.appendChild(linksList);
+
+                linksContainer.appendChild(weekContainer);
+            }
+        }
+
+        getLinks();

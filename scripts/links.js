202ebe47a984c6c7952736e4e99c38d0792f6d9f
GravesStone <est22003@byui.edu>
2023-11-08 11:34:17 +0800

    sync my files

--- scripts/links.js
+++ scripts/links.js
@@ -11,7 +11,6 @@ const baseURL = "https://gravesstone.github.io/wdd230/"; // Replace with your ac
             const linksContainer = document.getElementById('links-container');
 
             for (const week of weeks.lessons) {
-                const weekTitle = `Week ${week.lesson}`;
                 const linksList = document.createElement('ul');
 
                 for (const link of week.links) {
@@ -25,7 +24,7 @@ const baseURL = "https://gravesstone.github.io/wdd230/"; // Replace with your ac
 
                 const weekContainer = document.createElement('div');
                 weekContainer.className = 'week-container';
-                weekContainer.innerHTML = `<h2>${weekTitle}</h2>`;
+            
                 weekContainer.appendChild(linksList);
 
                 linksContainer.appendChild(weekContainer);

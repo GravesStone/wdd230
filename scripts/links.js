faee76fd3cdefa53d3fd3e4d26fdd6df8ac2a625
GravesStone <est22003@byui.edu>
2023-11-08 23:53:26 +0800

    test sync

--- scripts/links.js
+++ scripts/links.js
@@ -24,7 +24,7 @@ const baseURL = "https://gravesstone.github.io/wdd230/"; // Replace with your ac
                 }
 
                 const weekContainer = document.createElement('div');
-                weekContainer.className = 'week-container';
+              
                 weekContainer.innerHTML = `<p>${weekTitle}</p>`;
                 weekContainer.appendChild(linksList);
 

db2b25277a557710ef7154942f02ff05ecf78dec
GravesStone <est22003@byui.edu>
2023-11-10 22:49:11 +0800

    changes

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,59 +1,74 @@
 const baseURL = "https://gravesstone.github.io/wdd230/"; // Replace with your actual GitHub Pages URL
-        const linksURL = `${baseURL}chamber/data/membership.json`;
-async function getMembers() {
-  const response = await fetch(linksURL);
-  const data = await response.json();
-  displayMembers(data.members);
+const linksURL = `${baseURL}chamber/data/membership.json`;
+
+// Function to load member data from JSON asynchronously
+async function loadMemberData() {
+  try {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    return data.members;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
 }
 
-function displayMembers(members) {
-  const container = document.getElementById('directory-content');
-  container.innerHTML = '';
+// Function to display members in grid view
+function displayGridMembers(members) {
+  const directoryContent = document.getElementById("directory-content");
+  directoryContent.innerHTML = "";
 
-  const view = document.getElementById('view-toggle').value;
+  members.forEach((member) => {
+    const memberCard = document.createElement("div");
+    memberCard.className = "member-card";
 
-  members.forEach(member => {
-    if (view === 'grid') {
-      const card = document.createElement('div');
-      card.className = 'card';
-      card.innerHTML = `
-        <img src="${member.image}" alt="${member.name} image">
-        <div>
-          <h2>${member.name}</h2>
-          <p>${member.address}</p>
-          <p>Phone: ${member.phone}</p>
-          <p>Website: <a href="${member.website}" target="_blank">${member.website}</a></p>
-          <p>Membership Level: ${member.membershipLevel}</p>
-          <p>${member.otherInfo}</p>
-        </div>
-      `;
-      container.appendChild(card);
-    } else if (view === 'list') {
-      const listItem = document.createElement('div');
-      listItem.className = 'list-item';
-      listItem.innerHTML = `
-        <h2>${member.name}</h2>
-        <img src="${member.image}" alt="${member.name} image">
-        <div>
-          <p>${member.address}</p>
-          <p>Phone: ${member.phone}</p>
-          <p>Website: <a href="${member.website}" target="_blank">${member.website}</a></p>
-          <p>Membership Level: ${member.membershipLevel}</p>
-          <p>${member.otherInfo}</p>
-        </div>
-      `;
-      container.appendChild(listItem);
-    }
+    // Create member card content here
+    const cardContent = `
+      <h2>${member.name}</h2>
+      <p>Address: ${member.address}</p>
+      <p>Phone: ${member.phone}</p>
+      <p>Website: <a href="${member.website}" target="_blank">${member.website}</a></p>
+      <p>Membership Level: ${member.membershipLevel}</p>
+      <p>Other Info: ${member.otherInfo}</p>
+    `;
+
+    memberCard.innerHTML = cardContent;
+    directoryContent.appendChild(memberCard);
   });
 }
 
-function toggleView(selectedView) {
-  // Update the selected view
-  document.getElementById('view-toggle').value = selectedView;
-  
-  // Display members based on the selected view
-  displayMembers();
+// Function to display members in list view
+function displayListMembers(members) {
+  const directoryContent = document.getElementById("directory-content");
+  directoryContent.innerHTML = "<ul>";
+
+  members.forEach((member) => {
+    const listItem = document.createElement("li");
+    listItem.className = "list-item";
+    listItem.innerHTML = `
+      <h2>${member.name}</h2>
+      <p>Address: ${member.address}</p>
+      <p>Phone: ${member.phone}</p>
+      <p>Website: <a href="${member.website}" target="_blank">${member.website}</a></p>
+      <p>Membership Level: ${member.membershipLevel}</p>
+      <p>Other Info: ${member.otherInfo}</p>
+    `;
+    directoryContent.appendChild(listItem);
+  });
+
+  directoryContent.innerHTML += "</ul>";
 }
 
-// Call the async function to fetch members data
-getMembers();
+// Function to toggle between grid and list view
+async function toggleView(view) {
+  const membersData = await loadMemberData();
+
+  if (view === "grid") {
+    displayGridMembers(membersData);
+  } else if (view === "list") {
+    displayListMembers(membersData);
+  }
+}
+
+// Initially display members in grid view
+toggleView("grid");

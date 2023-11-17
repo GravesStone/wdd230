3b222cfb65e948d773eb3be5cba8f55e4a972f6c
GravesStone <est22003@byui.edu>
2023-11-17 23:19:51 +0800

    changes

--- chamber/scripts/spotlights.js
+++ chamber/scripts/spotlights.js
@@ -1,38 +1,64 @@
 const baseURL = "https://gravesstone.github.io/wdd230/"; // Replace with your actual GitHub Pages URL
 const linksURL = `${baseURL}chamber/data/members.json`;
 
+// Function to load member data from JSON asynchronously
 async function loadMemberData() {
-    try {
-      const response = await fetch(linksURL);
-      const data = await response.json();
-      return data.members;
-    } catch (error) {
-      console.error(error);
-      return [];
-    }
+  try {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    return data.members;
+  } catch (error) {
+    console.error(error);
+    return [];
   }
-  
-  function displayGridMembers(members) {
-    const directoryContent = document.getElementById("spotlights");
-    directoryContent.innerHTML = "";
-  
-    members.forEach((member) => {
-      const memberCard = document.createElement("section");
-      memberCard.className = "member-card";
-  
-      // Create member card content here
-      const cardContent = `
-        <h2>Companies</h2>
-        <img src="${member.image}" alt="${member.name} Image" class="member-image">
-        <p>Address: ${member.address}</p>
-        <p>Phone: ${member.phone}</p>
-        <p>Website: <a href="${member.website}" target="_blank">${member.website}</a></p>
-        <p>Membership Level: ${member.membershipLevel}</p>
-        <p>Other Info: ${member.otherInfo}</p>
-      `;
-  
-      memberCard.innerHTML = cardContent;
-      directoryContent.appendChild(memberCard);
-    });
-  }
-  
+}
+
+// Function to display members in grid view
+function displayGridMembers(members) {
+  const directoryContent = document.getElementById("directory-content");
+  directoryContent.innerHTML = "";
+
+  members.forEach((member) => {
+    const memberCard = document.createElement("div");
+    memberCard.className = "member-card";
+
+    // Create member card content here
+    const cardContent = `
+      <h2>${member.name}</h2>
+      <img src="${member.image}" alt="${member.name} Image" class="member-image">
+      <p>Address: ${member.address}</p>
+      <p>Phone: ${member.phone}</p>
+      <p>Website: <a href="${member.website}" target="_blank">${member.website}</a></p>
+      <p>Membership Level: ${member.membershipLevel}</p>
+      <p>Other Info: ${member.otherInfo}</p>
+    `;
+
+    memberCard.innerHTML = cardContent;
+    directoryContent.appendChild(memberCard);
+  });
+}
+
+// Function to display members in list view
+function displayListMembers(members) {
+  const directoryContent = document.getElementById("directory-content");
+  directoryContent.innerHTML = "<ul>";
+
+  members.forEach((member) => {
+    const listItem = document.createElement("li");
+    listItem.className = "list-item";
+    listItem.innerHTML = `
+      <h2>${member.name}</h2>
+      <img src="${member.image}" alt="${member.name} Image" class="member-image" width="100" height="100">
+      <p>Address: ${member.address}</p>
+      <p>Phone: ${member.phone}</p>
+      <p>Website: <a href="${member.website}" target="_blank">${member.website}</a></p>
+      <p>Membership Level: ${member.membershipLevel}</p>
+      <p>Other Info: ${member.otherInfo}</p>
+    `;
+    directoryContent.appendChild(listItem);
+  });
+
+  directoryContent.innerHTML += "</ul>";
+}
+
+// Function to toggle between grid and list view
--- chamber/scripts/transition.js
+++ chamber/scripts/transition.js
@@ -21,3 +21,13 @@ document.addEventListener('DOMContentLoaded', function() {
 	  }
 	});
   });
+  var linkButton = document.getElementById('linkbtn');
+
+  // Add a click event listener to the button
+  linkButton.addEventListener('click', function() {
+	  // Specify the link URL you want to navigate to
+	  var linkUrl = 'https://gravesstone.github.io/wdd230/chamber/join.html';
+
+	  // Navigate to the link URL
+	  window.location.href = linkUrl;
+  });

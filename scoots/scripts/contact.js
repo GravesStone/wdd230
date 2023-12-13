e01bba54950890e2672d3554a3651d75337ab16d
gravesstone <est2203@byui.edu>
2023-12-13 17:20:52 +0800

    changes

--- scoots/scripts/contact.js
+++ scoots/scripts/contact.js
@@ -1,10 +1,14 @@
 function submitForm() {
-    // You can add form validation logic here if needed
-    // For simplicity, this example assumes the form is valid
-
     // Get form data
     var name = document.getElementById("name").value;
-   
+    var email = document.getElementById("email").value;
+    var comment = document.getElementById("comment").value;
+
+    // Check if the name is empty
+    if (name.trim() === "") {
+        alert("Please enter your name before submitting the form.");
+        return;
+    }
 
     // Assuming the new page is "new-page.html"
     var redirectUrl = "feedback.html?name=" + encodeURIComponent(name);
